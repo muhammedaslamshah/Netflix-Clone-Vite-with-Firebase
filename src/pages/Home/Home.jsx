92f9c52d8b37de2b6d88bf137a89058cb06ba60d
muhammedaslamshah <muhammedaslamshah700@gmail.com>
2024-09-24 16:01:55 +0530

    Modified TitleCards && Home = css % jsx  for movie listing showcase

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,29 +1,52 @@
-import React from 'react'
-import './Home.css'
-import Navbar from '../../components/Navbar/Navbar'
-import hero_banner from '../../assets/hero_banner.jpg'
-import hero_title from '../../assets/hero_title.png'
-import play_icon from '../../assets/play_icon.png'
-import info_icon from '../../assets/info_icon.png'
+import React from "react";
+import "./Home.css";
+import Navbar from "../../components/Navbar/Navbar";
+import hero_banner from "../../assets/hero_banner.jpg";
+import hero_title from "../../assets/hero_title.png";
+import play_icon from "../../assets/play_icon.png";
+import info_icon from "../../assets/info_icon.png";
+import TitleCards from "../../components/TitleCards/TitleCards";
 
 const Home = () => {
   return (
-    <div className='home'>
-      <Navbar/>
+    <div className="home">
+      <Navbar />
       <div className="hero">
-        <img src={hero_banner} alt="" className='banner-img' />
+        <img src={hero_banner} alt="" className="banner-img" />
         <div className="hero-caption">
-          <img src={hero_title} alt="" className='caption-img' />
-          <p>Discovering his ties to a secret ancient order, a young man living in modern Istanbul embarks on quest to save the city an immortal enemy.</p>
-          <div className='hero-btns'>
-            <button className='btn'><img src={play_icon} alt="" />Play</button>
-            <button className='btn dark-btn'><img src={info_icon} alt="" />Play</button>
+          <img src={hero_title} alt="" className="caption-img" />
+          <p>
+            Discovering his ties to a secret ancient order, a young man living
+            in modern Istanbul embarks on quest to save the city an immortal
+            enemy.
+          </p>
+          {/* hero-btns Start */}
+          <div className="hero-btns">
+            <button className="btn">
+              <img src={play_icon} alt="" />
+              Play
+            </button>
+            <button className="btn dark-btn">
+              <img src={info_icon} alt="" />
+              More Info
+            </button>
           </div>
+          {/* hero-btns End */}
+
+          <TitleCards/>
         </div>
       </div>
+
+      {/* more-cards start */}
+      <div className="more-cards">
+        <TitleCards title={"Blockbuster Movies"} />
+        <TitleCards title={"Only on Netflix"}/>
+        <TitleCards title={"Upcoming"}/>
+        <TitleCards title={"Top Pics for You"}/>
+      </div>
+      {/* more-cards End */}
     </div>
-  )
-}
+  );
+};
 
-export default Home
-
+export default Home;
