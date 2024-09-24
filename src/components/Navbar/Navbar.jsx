03ce5911cf0c3f29677153686f8ef988b27cac55
muhammedaslamshah <muhammedaslamshah700@gmail.com>
2024-09-24 13:15:07 +0530

    Navbar.jsx layout complete before css design

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
-import './Navbar.css'
+import React from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
+import search_icon from "../../assets/search_icon.svg";
+import bell_icon from "../../assets/bell_icon.svg";
+import profile_img from "../../assets/profile_img.png";
+import caret_icon from "../../assets/caret_icon.svg";
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
-      Navbar
-    </div>
-  )
-}
+    //Navbar
+    <div className="navbar">
+      {/* Navbar left side */}
+      
+      <div className="navbar-left">
+        {/* Netflix Logo Img */}
+        <img src={logo} alt="" />
+        <ul>
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My List</li>
+          <li>Browse by Languages</li>
+        </ul>
+      </div>
 
-export default Navbar
+      {/* Navbar Right side */}
+      <div className="navbar-right">
+        <img src={search_icon} alt="" className="icons" />
+        <p>Children</p>
+        <img src={bell_icon} alt="" className="icons" />
+        <div className="navbar-profile">
+          <img src={profile_img} alt="" className="profile" />
+          <img src={caret_icon} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
